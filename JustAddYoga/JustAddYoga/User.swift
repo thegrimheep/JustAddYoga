c0a6148c9a540543f92f974c7cde63b250552aed
David <davidporter33@icloud.com>
2018-03-22 13:44:17 -0700

    Added user file if needed.

--- JustAddYoga/JustAddYoga/User.swift
+++ JustAddYoga/JustAddYoga/User.swift
@@ -8,4 +8,22 @@
 
 import Foundation
 
-
+class User {
+    let name: String
+    let profileImageUrl: String
+    let location: String
+    
+    init?(json: [String: Any]) {
+        if let name = json["name"] as? String,
+            let profileImageUrl = json["profile_image_url"] as? String,
+            let location = json["location"] as? String {
+            
+            self.name = name
+            self.profileImageUrl = profileImageUrl
+            self.location = location
+            
+        } else {
+            return nil
+        }
+    }
+}
